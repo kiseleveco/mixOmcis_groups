a7e8e8e40df865ec2425caff2da6c42e41329d65
kiseleveco <kiselev.eco@gmail.com>
2024-11-08 12:10:44 +0100

    initial commit

---
+++ mixOmics.R
@@ -0,0 +1,14 @@
+library(mixOmics)
+library(dplyr)
+library(tibble)
+library(janitor)
+library(fs)
+library(condvis)
+
+
+
+experiment <- list()
+
+workdir = path("/Users/kiselev/Documents/multiomics/mixOmics_groups/")
+
+
